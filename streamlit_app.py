129f1bf581ca65d4c824964494eb5174d88581fe
praneeth1906 <78490580+praneeth1906@users.noreply.github.com>
2023-02-22 19:38:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import snowflake.connector
+
 
 streamlit.title('My parents New Healthy Diner')
 #🥣 🥗 🐔 🥑🍞
@@ -36,6 +36,15 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Displaying the normalized data 
 streamlit.dataframe(fruityvice_normalized)
+
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
 

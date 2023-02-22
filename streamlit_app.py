098dbc221263d005d717b6222d5031511eb0ba09
praneeth1906 <78490580+praneeth1906@users.noreply.github.com>
2023-02-22 22:17:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error 
-import URLError
+from urllib.error import URLError
 
 
 streamlit.title('My parents New Healthy Diner')
